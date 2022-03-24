04642158fe5c17d0ae323bd9a529a642f69642e2
nofind <2868509713@qq.com>
2022-03-24 22:05:16 +0800

    Add pack function.

--- src/main/java/ZipHandler.java
+++ src/main/java/ZipHandler.java
@@ -1,5 +1,8 @@
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+
 import java.io.*;
-import java.nio.file.*;
+import java.nio.file.Files;
 import java.util.Scanner;
 import java.util.zip.*;
 
@@ -7,31 +10,65 @@ import java.util.zip.*;
  * @author dongyudeng
  */
 public class ZipHandler {
-    public static void unpack(String zipPath,String targetPath,String targetName) {
+    public static void unpack(String zipPath, String targetPath, String targetName) {
 
+    }
+
+    public static void pack(File nowFile, String nowFileName, ZipArchiveOutputStream zipOutputStream) throws IOException {
+        if (nowFile.isHidden()) {
+            return;
+        }
+        if (!nowFile.isDirectory()) {
+            ZipArchiveEntry entry = new ZipArchiveEntry(nowFileName);
+            zipOutputStream.putArchiveEntry(entry);
+            zipOutputStream.write(Files.readAllBytes(nowFile.toPath()));
+            zipOutputStream.closeArchiveEntry();
+        } else {
+            zipOutputStream.putArchiveEntry(new ZipArchiveEntry(nowFileName));
+            zipOutputStream.closeArchiveEntry();
+            File[] children = nowFile.listFiles();
+            if(children!=null){
+                for(File file:children){
+                    pack(file,nowFileName+"/"+file.getName(),zipOutputStream);
+                }
+            }
+        }
     }
 
     public static void pack(String sourcePath, String targetPath, String zipName) {
+        File sourceFile = new File(sourcePath), zipFile = new File(targetPath+"/"+zipName+".zip");
+        if (!sourceFile.exists() || zipFile.exists()) {
+            System.out.println("Error!");
+            return;
+        }
+        try (ZipArchiveOutputStream outputStream = new ZipArchiveOutputStream(zipFile)) {
+            pack(sourceFile, sourceFile.getName(), outputStream);
+        }catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
 
-    }
-    public static void doPack(){
-        Scanner scanner=new Scanner(System.in);
-        String sourcePath=scanner.nextLine(),targetPath=scanner.nextLine(),zipName=scanner.nextLine();
+    public static void doPack() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String sourcePath = scanner.nextLine(), targetPath = scanner.nextLine(), zipName = scanner.nextLine();
         pack(sourcePath, targetPath, zipName);
+        scanner.close();
     }
-    public static void doUnpack(){
-        Scanner scanner=new Scanner(System.in);
-        String zipPath=scanner.nextLine(),targetPath=scanner.nextLine(),targetName=scanner.nextLine();
+
+    public static void doUnpack() {
+        Scanner scanner = new Scanner(System.in);
+        String zipPath = scanner.nextLine(), targetPath = scanner.nextLine(), targetName = scanner.nextLine();
         unpack(zipPath, targetPath, targetName);
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String operation = scanner.nextLine();
-        scanner.close();
         switch (operation) {
             case "pack" -> doPack();
             case "unpack" -> doUnpack();
             default -> System.out.println("unknown operation.");
         }
+        scanner.close();
     }
 }
